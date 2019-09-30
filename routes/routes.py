74498e9f35d913cdac13ae3d09ff2a6ea7fbe35f
Sameer <sameer.jain@rsa.com>
2019-09-30 21:54:13 +0530

    Add functionality for add operation

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 router = Blueprint("router", __name__)
 
@@ -8,5 +8,13 @@ def check():
 
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return
+    """API that returns sum of given list of numbers
+
+    Input: {'numbers':[1,2,3,4]}
+    
+    Returns:
+        str -- sum of numbers in string format
+    """
+    data = request.json
+    numbers = data.get('numbers', [])
+    return str(sum(numbers))
