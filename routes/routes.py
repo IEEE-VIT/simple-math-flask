a8269803785be3f5dcdb2d1c7c2f8b83aa546ec7
Sunandan Chakrabarti <43108937+SunandanChakrabarti@users.noreply.github.com>
2019-10-01 15:08:26 -0700

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -10,3 +10,15 @@ def check():
 def add():
     # Add logic here
     return
+    x = request.form.data.param1
+    y = request.form.data.param2
+
+    summation = x+y
+
+    res = {
+        result: summation,
+        meta: {
+            message: "sending addition of two numbers"
+        }
+    }
+    return res
