3e847a3f0bf67429700079b38a8d87650c288444
Rishi Raj Singh <creator@pop-os.localdomain>
2019-10-01 22:26:39 +0530

    added route for adding two numbers from the form field

--- routes/routes.py
+++ routes/routes.py
@@ -2,11 +2,24 @@ from flask import Blueprint
 
 router = Blueprint("router", __name__)
 
+
 @router.route("/check")
 def check():
     return "Congratulations! Your app works. :)"
 
+
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
+
+    x = request.form.data.param1
+    y = request.form.data.param2
+    
+    summation = x+y
+    
+    res = {
+        result: summation,
+        meta: {
+            message: "sending addition of two numbers"
+        }
+    }
     return
