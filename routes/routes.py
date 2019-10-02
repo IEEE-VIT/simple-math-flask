83fedcf5ac11975d7c5cd6a9f76d3ff13be2923e
Dmitriy Kharavets <kharavets123@mail.ru>
2019-10-02 11:31:21 +0300

    Added logarithm route and tests for it.

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, json
+from math import log, exp
 
 router = Blueprint("router", __name__)
 
@@ -10,3 +11,25 @@ def check():
 def add():
     # Add logic here
     return
+
+
+@router.route("/logarithm", methods=["POST"])
+def logarithm():
+    """ Returns a logarithm of a number with desired base.
+    Args:
+        number (:obj:`float`): A number to calculate logarithm for.
+        base (:obj:`int`): Base of a logarithm. Optional argument. If omitted, function returns a natural logarithm
+        of a given number.
+    """
+    data = request.json
+    print(data)
+
+    if not data:
+        return json.dumps({'error': 'No input was provided.'})
+
+    number = data.get("number")
+    base = data.get("base", None)
+    if not number:
+        return json.dumps({'error': 'No number to calculate a logarithm was provided.'})
+
+    return {"result": log(number, base)} if base else {"result": log(number)}
