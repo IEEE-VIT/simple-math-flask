424b71f961a70fb64eb656dbe9196caa199573c4
Srinivas <nandusrinivas33.cse@gmail.com>
2019-10-02 11:45:38 +0530

    add power functionality and tests

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
 
 router = Blueprint("router", __name__)
 
@@ -10,3 +10,20 @@ def check():
 def add():
     # Add logic here
     return
+
+@router.route("/power", methods=["POST"])
+def power():
+    data = request.json
+    if not data:
+        return make_response(jsonify({
+            'error': "Input expected!"
+        }), 400)
+    
+    base = request.json.get("base")
+    exponent = request.json.get("exponent", 0)
+    if not base:
+        return make_response(jsonify({
+            "error": "Base value expected!"
+        }), 400)
+    
+    return { "result": base ** exponent }
--- test/test_app.py
+++ test/test_app.py
@@ -18,5 +18,23 @@ class AppTests(unittest.TestCase):
         response = self.app.get('/math/check')
         self.assertEqual(response.data, b'Congratulations! Your app works. :)')
 
+    def test_power_without_data(self):
+        response = self.app.post('/math/power')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json["error"], "Input expected!")
+    
+    def test_power_without_base_value(self):
+        response = self.app.post('/math/power', json={"exponent": 1})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json["error"], "Base value expected!")
+    
+    def test_power_without_exponent_value(self):
+        response = self.app.post('/math/power', json={"base": 2})
+        self.assertEqual(response.json["result"], 1)
+
+    def test_power_success(self):
+        response  = self.app.post('/math/power', json={"base": 2, "exponent": 3})
+        self.assertEqual(response.json["result"], 8)
+
 if __name__ == '__main__':
     unittest.main()
