ee60bfb4f866ce21157ec8e5f69e763e89f932c5
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2019-10-02 10:49:01 +0530

    Wrote functionality for multiplication and division
    Multiplication works with an arrayy, and division works with 2 arguments.

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint,request
+import json, functools
 
 router = Blueprint("router", __name__)
 
@@ -10,3 +11,40 @@ def check():
 def add():
     # Add logic here
     return
+
+@router.route("/product", methods=["POST"])
+def product():
+    result = {
+        'result': None,
+        'meta': None
+    }
+
+    # Works with multiple parameters
+
+    if request.get_json():
+        args = json.loads(request.get_json())
+        args = [args['data'][i] for i in args['data']]
+        result['result'] = functools.reduce(lambda x,y: x*y,args)
+        return json.dumps(result)
+
+    else:
+        result['meta'] = 'No JSON provided'
+        return json.dumps(result)
+
+@router.route("/division", methods=["POST"])
+def division():
+    result = {
+        'result': None,
+        'meta': None
+    }
+
+    # Works with 2 parameters
+
+    if request.get_json():
+        args = json.loads(request.get_json())['data']
+        result['result'] = args['param1']/args['param2']
+        return json.dumps(result)
+
+    else:
+        result['meta'] = 'No JSON provided'
+        return json.dumps(result)
