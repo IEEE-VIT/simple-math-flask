dab8db30d3c80603ae4ac5a759ab70f954039384
aryan <aryan@Aryans-MacBook-Pro.local>
2019-10-04 10:41:20 +0530

    Added adding functionality

--- routes/routes.py
+++ routes/routes.py
@@ -1,12 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 router = Blueprint("router", __name__)
+
 
 @router.route("/check")
 def check():
     return "Congratulations! Your app works. :)"
 
+
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return
+    first_number = request.form['FirstNumber']
+    second_number = request.form['SecondNumber']
+    result = first_number + second_number
+    return result
