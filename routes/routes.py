7f85eaa329cd79f8a8b5ccef3ffc5341ef592615
rushivarun <varun.muniaplle2017@vitstudent.ac.in>
2019-10-07 15:48:07 +0530

    product, diff, sum added

--- routes/routes.py
+++ routes/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import jsonify, request
+from flask import jsonify, request, Response
 router = Blueprint("router", __name__)
 
 @router.route("/check")
@@ -22,9 +22,54 @@ def add():
         result['result'] = sum(values)
     return jsonify(result)
 
-# @ router.route('/subtract', methods = ['POST'])
-# def subtract():
-#     data_in = request.json['data']
-#     values = []
-#     for i in data_in:
+@ router.route('/subtract', methods = ['POST'])
+def subtract():
+    result = {
+        'result' : None,
+        'meta' : None
+    }
+    if not request.json:
+        result['meta'] = "please post some values"
+        return jsonify(result)
+    else:
+        data_in = request.json['data']
+
+    if not data_in:
+        result['meta'] = "no json data was provided.."
+        return jsonify(result)
+    else:
+        values = []
+        for i in data_in:
+            values.append(data_in[i])
+    result['result'] = 0
+    for i in values:
+        result['result'] = i - result['result']
+    return jsonify(result)
+
+@router.route('/product', methods = ['POST'])
+def product():
+    result = {
+        'result' : None,
+        'meta' : None
+    }
+    if not request.json:
+        result['meta'] = "please post some values"
+        return jsonify(result)
+    else:
+        data_in = request.json['data']
+
+    if not data_in:
+        result['meta'] = "no json data was provided.."
+        return jsonify(result)
+    else:
+        values = []
+        for i in data_in:
+            values.append(data_in[i])
+    result['result'] = 1
+    for i in values:
+        result['result'] = i * result['result']
+    
+    return jsonify(result)
+
+
         
