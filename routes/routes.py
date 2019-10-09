dae324a38525ba7b8323722d7e616c39532259b7
rushivarun <varun.muniaplle2017@vitstudent.ac.in>
2019-10-10 00:13:38 +0530

    matrix subtraction active

--- routes/routes.py
+++ routes/routes.py
@@ -103,4 +103,35 @@ def mat_add():
     result['result'] = result_t
     print(str(result))
     return jsonify(str(result))
+
+@router.route('/mat_sub', methods = ['POST'])
+def mat_sub():
+    result = {
+        'result' : None,
+        'meta' : None
+    }
+    if not request.json:
+        result['meta'] = "please post some values"
+        return jsonify(result)
+    else:
+        data_in = request.json['data']
+
+    if not data_in:
+        result['meta'] = "no json data was provided.."
+        return jsonify(result)
+    else:
+        values = None
+        for i in data_in:
+            values = data_in[i]
+
+    sh = np.shape(values)
+    zero_mat = np.zeros(sh, dtype = int)
+    result_t = zero_mat[0]
+
+    for k in values:
+        result_t = [[k[i][j] - result_t[i][j]  for j in range(len(k[0]))] for i in range(len(k))]
+    result_t
+    result['result'] = result_t
+    print(str(result))
+    return jsonify(str(result))
             
