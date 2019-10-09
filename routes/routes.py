c23b46ce41aa97ec6681eedb060d5e6fb93405a9
rushivarun <varun.muniaplle2017@vitstudent.ac.in>
2019-10-10 00:17:53 +0530

    matrix addition active

--- routes/routes.py
+++ routes/routes.py
@@ -134,4 +134,37 @@ def mat_sub():
     result['result'] = result_t
     print(str(result))
     return jsonify(str(result))
+
+@router.route('/mat_mul', methods = ['POST'])
+def mat_mul():
+    result = {
+        'result' : None,
+        'meta' : None
+    }
+    if not request.json:
+        result['meta'] = "please post some values"
+        return jsonify(result)
+    else:
+        data_in = request.json['data']
+
+    if not data_in:
+        result['meta'] = "no json data was provided.."
+        return jsonify(result)
+    else:
+        values = None
+        for i in data_in:
+            values = data_in[i]
+
+    sh = np.shape(values)
+    one_mat = np.ones(sh, dtype = int)
+    result_t = one_mat[0]
+
+    for k in values:
+        result_t = [[k[i][j] * result_t[i][j]  for j in range(len(k[0]))] for i in range(len(k))]
+    result_t
+    result['result'] = result_t
+    print(str(result))
+    return jsonify(str(result))
+
+
             
