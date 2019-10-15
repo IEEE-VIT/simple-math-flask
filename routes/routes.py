4578164a5b30ede11d18312bac34a1eab4fd80c9
antirex <55479324+antirex@users.noreply.github.com>
2019-10-15 20:50:24 +0530

    Sum of an infinite Arithmetic-Geometric Progression

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,12 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
-@router.route("/add", methods=["POST"])
-def add():
-    # Add logic here
-    return
+@router.route("/AGP", methods=["POST"])
+def AGP():
+    r=int(input("enter the value of common ratio "))
+    a=int(input("enter the vlaue of first term"))
+    d=int(input("enter the value of the common difference"))
+    sum1=a/(1-r)
+    sum2=(d*r)/(1-r)**2
+    total=sum1+sum2
+    return(total)
