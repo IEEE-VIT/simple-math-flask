11b75036f08223971bf36f45b3e6045b4a14ca62
includeavaneesh <54032418+includeavaneesh@users.noreply.github.com>
2019-10-15 16:03:26 +0530

    Added functionality of least common multiple
    calculates the least common multiple of two numbers

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,23 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
-@router.route("/add", methods=["POST"])
-def add():
-    # Add logic here
-    return
+@router.route("/least_common_multiple", methods=["POST"])
+def least_common_multiple():
+    a=int(input("Enter first number"))
+    b=int(input("Enter second number"))
+   
+    g=a
+    if b>=a:
+        g=b
+        
+    l=a*b+1  
+    
+    for i in range(g,l,1):
+        if i%a==0 and i%b==0:
+            break
+            
+    return i
+
+
+
+                
