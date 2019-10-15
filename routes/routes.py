b2d12db6ed72716c5ceb19fde0dbbf89c1c2f190
antirex <55479324+antirex@users.noreply.github.com>
2019-10-15 18:35:53 +0530

    Calculating the sum of an infinite GP

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,13 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
-@router.route("/add", methods=["POST"])
-def add():
-    # Add logic here
-    return
+@router.route("infiniteGP", methods=["POST"])
+def infiniteGP():
+   a=int(input("enter the first term of the infinite GP"))
+   r=int(input("enter the value of common ratio"))
+   if(r==1):
+        print("invalid input please try again")
+   else:
+        s=a/(1-r)
+
+    return(s)
