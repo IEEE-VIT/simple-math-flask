484c51ae262d3efc66999d0d05c253d03b2d6a84
antirex <55479324+antirex@users.noreply.github.com>
2019-10-15 18:52:42 +0530

    Calculate the sum of 'n' numbers

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,9 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
-@router.route("/add", methods=["POST"])
-def add():
-    # Add logic here
-    return
+@router.route("/summation", methods=["POST"])
+def summation():
+    n=int(input("enter the value of numbers "))
+    s1=(n*(n+1))/2 #sum of n numbers 
+
+    return(s1)
