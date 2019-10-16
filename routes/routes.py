020794ef8cfdd85a6cd776c49818aedf35352b07
unknown <agrimnautiyal1999@gmail.com>
2019-10-17 03:30:47 +0530

    Added 4 routes and linked index to first route

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,36 @@ router = Blueprint("router", __name__)
 def start():
     return render_template('index.html')
 
+
+#function to add
 @router.route("/add", methods=["POST"])
 def add():
     # Add logic here
-    return
+    n1 = int(request.form['num1'])
+    n2 = int(request.form['num2'])
+    return n1+n2
+
+
+#function to subtract
+@router.route("/sub", methods=["POST"])
+def add():
+    # Add logic here
+    n1 = int(request.form['num1'])
+    n2 = int(request.form['num2'])
+    return n1-n2
+
+#function to multiply
+@router.route("/mul", methods=["POST"])
+def add():
+    # Add logic here
+    n1 = int(request.form['num1'])
+    n2 = int(request.form['num2'])
+    return n1*n2
+
+#function to divide (integer division)
+@router.route("/div", methods=["POST"])
+def add():
+    # Add logic here
+    n1 = int(request.form['num1'])
+    n2 = int(request.form['num2'])
+    return n1//n2
