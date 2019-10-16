a0de6552d329f5388653aafb05b827536a1b00e9
Dev-Shankar-Paul <56624437+Dev-Shankar-Paul@users.noreply.github.com>
2019-10-16 12:14:51 +0530

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -3,8 +3,13 @@ from flask import Blueprint
 router = Blueprint("router", __name__)
 
 @router.route("/check")
-def check():
+def check():                     # creating a UDF(User Defined Function) named check
     return "Congratulations! Your app works. :)"
+
+# DSP made this change for a demo
+@router.route("/hello")
+def hello():
+    return "Hacktoberfest is cool!"     # prints "Hacktoberfest is cool" on the output screen
 
 @router.route("/add", methods=["POST"])
 def add():
