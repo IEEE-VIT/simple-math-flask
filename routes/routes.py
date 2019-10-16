132315eaf83a8ee08bddb378781a1af31535c722
sanjaybabu2210 <42745546+sanjaybabu2210@users.noreply.github.com>
2019-10-16 12:14:41 +0530

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -6,6 +6,11 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
+#sanjay made this changes
+@router.route("/hello")
+def hello():
+    return "Hactober is cool!. :)"
+
 @router.route("/add", methods=["POST"])
 def add():
     # Add logic here
