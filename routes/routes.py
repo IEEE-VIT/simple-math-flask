90cd9a7fd357db0de1eebfba1882ac9bc0e521a0
Anuraj0124 <56624675+Anuraj0124@users.noreply.github.com>
2019-10-16 12:17:19 +0530

    Update routes.py
    Anuraj0124 opened this PR for hactober fest.

--- routes/routes.py
+++ routes/routes.py
@@ -1,10 +1,13 @@
-from flask import Blueprint
+From flask import Blueprint
 
 router = Blueprint("router", __name__)
 
 @router.route("/check")
 def check():
     return "Congratulations! Your app works. :)"
+@router.route("/hello")
+def hello():
+    return "Hacktober is cool."
 
 @router.route("/add", methods=["POST"])
 def add():
