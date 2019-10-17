e880417ea7645ff320fd5395e3dae5489dc11614
Priyesh Bhawsar <priyeshbhawsar15@gmail.com>
2019-10-17 22:20:10 +0530

    added a subtraction route

--- routes/routes.py
+++ routes/routes.py
@@ -6,7 +6,8 @@ router = Blueprint("router", __name__)
 def check():
     return "Congratulations! Your app works. :)"
 
-@router.route("/add", methods=["POST"])
-def add():
-    # Add logic here
-    return
+def subtract():
+    val1 = request.form['val1']
+    val2 = request.form['val2']
+    add = val1 + val2
+    return flask.jsonify({'data':f'<p>The result is: {val1-val2}</p>'})
