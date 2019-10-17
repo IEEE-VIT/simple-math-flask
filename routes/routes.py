4509a1cd56b6220deb80d1fb09036bca20f24720
Priyesh Bhawsar <priyeshbhawsar15@gmail.com>
2019-10-17 15:33:51 +0530

    addtion route

--- routes/routes.py
+++ routes/routes.py
@@ -8,5 +8,7 @@ def check():
 
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return
+    val1 = request.form['val1']
+    val2 = request.form['val2']
+    add = val1 + val2
+    return flask.jsonify({'data':f'<p>The result is: {val1+val2}</p>'})
