de90a117b230baa1950c68f5de0b5052cc61ee9b
Neda Peyrone <peyrone.n@gmail.com>
2019-10-20 04:44:58 +0700

    Add Dockerfile

--- .history/app_20191020042550.py
+++ .history/app_20191020042550.py
@@ -10,4 +10,5 @@ CORS(app)
 app.register_blueprint(router, url_prefix="/math")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(port=PORT)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- .history/app_20191020042553.py
+++ .history/app_20191020042553.py
@@ -10,4 +10,4 @@ CORS(app)
 app.register_blueprint(router, url_prefix="/math")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
