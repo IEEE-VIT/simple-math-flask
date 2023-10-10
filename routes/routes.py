3766b871257261a254b9bf0d94d6814a6ed0de0c
KeziaWahome <k.wahome@alustudent.com>
2023-10-10 23:05:22 +0200

    update: added the multiplication function and it's test unit

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 router = Blueprint("router", __name__)
 
@@ -8,15 +8,35 @@ def check():
 
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return 
+    try:
+        data = request.get_json()
+        num1 = data.get("num1")
+        num2 = data.get("num2")
+        result = num1 + num2
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
-@router.route("multiply",methods=["POST"])
+@router.route("/multiply", methods=["POST"])
 def multiply():
-    #Add logic here
-    return
+    try:
+        data = request.get_json()
+        num1 = data.get("num1")
+        num2 = data.get("num2")
+        result = num1 * num2
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
-@router.route("/division",methods=['POST'])
+@router.route("/division", methods=['POST'])
 def division():
-    #Add logic here
-    return
+    try:
+        data = request.get_json()
+        num1 = data.get("num1")
+        num2 = data.get("num2")
+        if num2 == 0:
+            return jsonify({"error": "Division by zero is not allowed"}), 400
+        result = num1 / num2
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,9 @@
-import sys,os
+import sys
+import os
 sys.path.append(os.getcwd())
 
 from app import app
-import os 
+import os
 import unittest
 
 class AppTests(unittest.TestCase):
@@ -18,7 +19,17 @@ class AppTests(unittest.TestCase):
         response = self.app.get('/math/check')
         self.assertEqual(response.data, b'Congratulations! Your app works. :)')
 
-    #Add the test_cases for various functionality here
+    def test_addition(self):
+        data = {"num1": 3, "num2": 4}
+        response = self.app.post('/math/add', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"result": 7})
+
+    def test_multiplication(self):
+        data = {"num1": 2, "num2": 5}
+        response = self.app.post('/math/multiply', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"result": 10})
 
 if __name__ == '__main__':
     unittest.main()
