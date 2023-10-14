f155cb52553892e91ef2a78518704e6bf1d3fb97
Dennis Thomas <40732639+DNA5769@users.noreply.github.com>
2023-10-14 17:14:01 +0530

    Added matrix addition functionality (#109)
    * Added route for matrix addition
    
    * Checking if request body to matrix addition route is of correct format
    
    * Added test for request with no json body
    
    * - Checking if data is a dictionary
    - Checking if there are atleast 2 operands
    - Wrote appropriate tests
    
    * Ensuring parameters are named properly in request body and wrote test
    for it
    
    * Checking if operands are proper for matrix addition and making tests
    
    * Implemented matrix addition and added tests

--- test/test_app.py
+++ test/test_app.py
@@ -20,5 +20,65 @@ class AppTests(unittest.TestCase):
 
     #Add the test_cases for various functionality here
 
+    def test_matrix_addition_request_format1(self):
+        response = self.app.post('/math/matrixaddition')
+        self.assertEqual(response.json['meta']['error'], 'The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }')
+    
+    def test_matrix_addition_request_format1(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": { "foo": "bar" } })
+        self.assertEqual(response.json['meta']['error'], 'The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }')
+
+    def test_matrix_addition_request_format3(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {} })
+        self.assertEqual(response.json['meta']['error'], 'Matrix Addition requires atleast 2 operands')
+
+    def test_matrix_addition_operand_format1(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {
+            "param1": "foo",
+            "param2": "bar"
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Operands should be a matrix i.e list of lists of integers/floats')
+    
+    def test_matrix_addition_operand_format2(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {
+            "param1": ["foo"],
+            "param2": ["bar"]
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Operands should be a matrix i.e list of lists of integers/floats')
+    
+    def test_matrix_addition_operand_format3(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {
+            "param1": [["foo"]],
+            "param2": [["bar"]]
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Operands should be a matrix i.e list of lists of integers/floats')
+    
+    def test_matrix_addition_operand_format4(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {
+            "param1": [[1], [2], [3]],
+            "param2": [[1, 2, 3]]
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Operands of matrix addition should be of same dimensions nxm')
+    
+    def test_matrix_addition_correctness(self):
+        response = self.app.post('/math/matrixaddition', json={ "data": {
+            "param1": [
+                [1, 1],
+                [1, 1]
+            ],
+            "param2": [
+                [2, 2],
+                [2, 2]
+            ],
+            "param3": [
+                [3, 3],
+                [3, 3]
+            ],
+        } })
+        self.assertEqual(response.json['result'], [
+            [6, 6],
+            [6, 6]
+        ])
+
 if __name__ == '__main__':
     unittest.main()
