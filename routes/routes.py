b0a58dce88894363ac0cfe880202eb0af8d80195
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2023-10-23 23:13:58 +0530

    route math/add

--- routes/routes.py
+++ routes/routes.py
@@ -10,8 +10,51 @@ def check():
 
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return 
+    # Checking if request body is of correct format
+    try:
+        body = request.json
+        data = body['data']
+        assert isinstance(data, dict)
+    except Exception:
+        return jsonify({
+            "result": None,
+            "meta": { "error": "The request must be a JSON of the following format: { data: { param1: <value>, param2: <value> } }" }
+        }), HTTPStatus.BAD_REQUEST
+    
+    # Checking if operand keys in data dictionary is named properly
+    for i in range(1, len(data.keys())+1):
+        if f"param{i}" not in data:
+            return jsonify({
+                "result": None,
+                "meta": { "error": "The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }" }
+            }), HTTPStatus.BAD_REQUEST
+    
+    # Checking if there are 2 operands
+    if len(data.keys()) != 2:
+        return jsonify({
+            "result": None,
+            "meta": { "error": "The request must contain exactly two operands." }
+        }), HTTPStatus.BAD_REQUEST
+
+    # Checking if operand is of the correct format
+    try:
+        assert isinstance(data["param1"], int) or isinstance(data["param1"], float)
+        assert isinstance(data["param2"], int) or isinstance(data["param2"], float)
+        
+    except Exception:
+        return jsonify({
+            "result": None,
+            "meta": {"error": "Operands param1 and param2 should be either an int or a float"}
+        }), HTTPStatus.BAD_REQUEST
+    
+    
+    # Result of Addition
+    result = data["param1"] + data["param2"]
+    
+    return jsonify({
+        "result": result,
+        "meta": {}
+    }), HTTPStatus.OK
 
 @router.route("multiply",methods=["POST"])
 def multiply():
