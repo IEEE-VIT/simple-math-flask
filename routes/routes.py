0cd344e8097af0d5c46e000dd9a20c43b36ed42d
Achintya Bhat <72146263+docjon09@users.noreply.github.com>
2023-10-23 15:35:12 +0530

    Added division functionality (#113)
    * Added division functionality
    
    * Changed division function name
    
    * Update routes/routes.py
    
    Co-authored-by: GOPAL-YADAV-D <130576399+GOPAL-YADAV-D@users.noreply.github.com>
    
    * Resolved wrong function name in test_app.py
    
    ---------
    
    Co-authored-by: GOPAL-YADAV-D <130576399+GOPAL-YADAV-D@users.noreply.github.com>

--- routes/routes.py
+++ routes/routes.py
@@ -57,8 +57,54 @@ def multiply():
 
 @router.route("/division",methods=['POST'])
 def division():
-    #Add logic here
-    return
+    # Checking if request body is of correct format
+    try:
+        body = request.json
+        data = body['data']
+        assert isinstance(data, dict)
+    except Exception:
+        return jsonify({
+            "result": None,
+            "meta": { "error": "The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }" }
+        }), HTTPStatus.BAD_REQUEST
+    
+    # Checking if operand keys in data dictionary is named properly
+    for i in range(1, len(data.keys())+1):
+        if f"param{i}" not in data:
+            return jsonify({
+                "result": None,
+                "meta": { "error": "The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }" }
+            }), HTTPStatus.BAD_REQUEST
+    
+    # Checking if there are exactly 2 operands
+    if len(data.keys()) != 2:
+        return jsonify({
+            "result": None,
+            "meta": { "error": "Division requires exactly 2 operands" }
+        }), HTTPStatus.BAD_REQUEST
+
+    # Checking if operand is of the correct format
+    for i in range(1, len(data.keys())+1):
+        if not isinstance(data[f'param{i}'], int) and not isinstance(data[f'param{i}'], float):
+            return jsonify({
+                "result": None,
+                "meta": { "error": "Operands should be a real number i.e. integer/float" }
+            }), HTTPStatus.BAD_REQUEST
+    
+    # Check if the divisor is zero
+    if data['param2'] == 0:
+        return jsonify({
+            "result": None,
+            "meta": { "error": "Division by zero is not allowed" }
+        }), HTTPStatus.BAD_REQUEST
+    
+    div_res = data["param1"] / data["param2"]
+
+    return jsonify({
+        "result": div_res,
+        "meta": {}
+    }), HTTPStatus.OK
+
 
 @router.route("/exponentiation", methods=['POST'])
 def exponentiation():
--- test/test_app.py
+++ test/test_app.py
@@ -46,6 +46,43 @@ class AppTests(unittest.TestCase):
         } })
         self.assertEqual(response.json['result'], 20)
 
+    def test_division_request_format1(self):
+        response = self.app.post('/math/division')
+        self.assertEqual(response.json['meta']['error'], 'The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }')
+    
+    def test_division_request_format2(self):
+        response = self.app.post('/math/division', json={ "data": { "foo": "bar" } })
+        self.assertEqual(response.json['meta']['error'], 'The request must be a JSON of the following format: { data: { param1: <value>, ... param<n>: <value> } }')
+
+    def test_division_request_format3(self):
+        response = self.app.post('/math/division', json={ "data": {"param1":1} })
+        self.assertEqual(response.json['meta']['error'], 'Division requires exactly 2 operands')
+    
+    def test_division_request_format4(self):
+        response = self.app.post('/math/division', json={ "data": {"param1":1, "param2":2, "param3":3} })
+        self.assertEqual(response.json['meta']['error'], 'Division requires exactly 2 operands')
+    
+    def test_division_operand_format1(self):
+        response = self.app.post('/math/division', json={ "data": {
+            "param1": "foo",
+            "param2": "bar"
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Operands should be a real number i.e. integer/float')
+    
+    def test_division_operand_format2(self):
+        response = self.app.post('/math/division', json={ "data": {
+            "param1": 1,
+            "param2": 0
+        } })
+        self.assertEqual(response.json['meta']['error'], 'Division by zero is not allowed')
+    
+    def test_division_correctness(self):
+        response = self.app.post('/math/division', json={ "data": {
+            "param1": 3,
+            "param2": 2,
+        } })
+        self.assertEqual(response.json['result'], 1.5)
+
     def test_exponentiation_request_format1(self):
         response = self.app.post('/math/exponentiation')
         self.assertEqual(response.json['meta']['error'], 'The request must be a JSON of the following format: { data: { param1: <value>, param2: <value> } }')
