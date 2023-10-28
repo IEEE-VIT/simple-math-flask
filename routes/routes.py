4a294224f1786af6a1c9d4932a15b67df00ac900
AimenYaseen <aimenyaseen6@gmail.com>
2023-10-28 10:57:37 +0500

    called multiply_matrices view method in routes

--- routes/routes.py
+++ routes/routes.py
@@ -39,77 +39,7 @@ def matrix_addition():
 
 @router.route("/matrixmultiplication", methods=['POST'])
 def matrix_multiplication():
-    """
-    Multiply two or more matrices and return the resultant matrix.
-    It takes a list of matrices in request body. Each matrix should
-    be a list of lists and contains only int or float values.
-    """
-    # Checking if request body is of correct format
-    try:
-        body = request.json
-        matrices = body['matrices']
-        assert isinstance(matrices, list)
-    except Exception:
-        return jsonify({
-            "result": None,
-            "meta": {
-                "error": "The request must be a JSON of the following format: { matrices: [matrix_1, matrix_2, ..., "
-                         "matrix_n] } Here matrix_1, matrix_2, ..., matrix_n must be list of lists"}
-        }), HTTPStatus.BAD_REQUEST
-
-    # Checking if there are at least 2 matrices
-    if len(matrices) < 2:
-        return jsonify({
-            "result": None,
-            "meta": {"error": "At least two matrices are required for multiplication"}
-        }), HTTPStatus.BAD_REQUEST
-
-    # Checking if operand is of the correct format
-    for matrix in matrices:
-        if not isinstance(matrix, list) or not matrix:
-            return jsonify({
-                "result": None,
-                "meta": {"error": "A matrix can't be empty or a matrix should be a list of lists of integers/floats"}
-            }), HTTPStatus.BAD_REQUEST
-
-        for row in matrix:
-            if not isinstance(row, list) or len(row) == 0:
-                return jsonify({
-                    "result": None,
-                    "meta": {"error": "A matrix row can't be Empty or A matrix row should be a list"}
-                }), HTTPStatus.BAD_REQUEST
-
-            for el in row:
-                if not isinstance(el, int) and not isinstance(el, float):
-                    return jsonify({
-                        "result": None,
-                        "meta": {"error": "A matrix row should contain either an int or float"}
-                    }), HTTPStatus.BAD_REQUEST
-
-    matrix_a = matrices[0]
-    for matrix_b in matrices[1:]:
-        rows_a, cols_a = len(matrix_a), len(matrix_a[0])
-        rows_b, cols_b = len(matrix_b), len(matrix_b[0])
-
-        if cols_a != rows_b:
-            return jsonify({
-                "result": None,
-                "meta": {"error": "Matrix dimensions are not compatible for multiplication! i.e. The columns of first"
-                                  " matrix should be equal to the rows of 2nd matrix and so on."}
-            }), HTTPStatus.BAD_REQUEST
-
-        _result = [[0 for _ in range(cols_b)] for _ in range(rows_a)]
-
-        for i in range(rows_a):
-            for j in range(cols_b):
-                for k in range(cols_a):
-                    _result[i][j] += matrix_a[i][k] * matrix_b[k][j]
-        matrix_a = _result
-
-    return jsonify({
-        "result": matrix_a,
-        "meta": {}
-    }), HTTPStatus.OK
+    return MultiplyViews.multiply_matrices()
 
 
 @router.route("/quadraticequation", methods=['POST'])
